39affa0531cceda3890fb4c25c223dfe5a24dff4
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2022-02-21 03:35:58 -0600

    initial commit

---
+++ signup/urls.py
@@ -0,0 +1,27 @@
+"""signup URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+from accounts.views import home_view, signup_view, activation_sent_view, activate
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home_view, name="home"),
+    path('signup/', signup_view, name="signup"),
+    path('sent/', activation_sent_view, name="activation_sent"),
+    path('activate/<slug:uidb64>/<slug:token>/', activate, name='activate'),
+]
