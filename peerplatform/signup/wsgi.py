eed8136feb775f70c5a7443cc6af50a828760f02
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2022-02-24 17:37:33 -0600

    react home page

--- peerplatform/signup/wsgi.py
+++ peerplatform/signup/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for signup project.
+WSGI config for signup_components project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signup.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signup_components.settings')
 
 application = get_wsgi_application()
