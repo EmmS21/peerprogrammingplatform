df3ff6336b629bcd102803d0d1f6344a3e3798ba
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2022-09-01 13:40:43 -0500

    set up first unit test

--- peerplatform/peerplatform-fe/src/tests/userLogin.test.js
+++ peerplatform/peerplatform-fe/src/tests/userLogin.test.js
@@ -4,13 +4,19 @@ import Login from '../components/login_components/Login';
 import Signup from '../components/signup_components/Signup';
 import { BrowserRouter as Router } from "react-router-dom"
 import { SnackbarProvider } from "notistack"
+import Home from '../views/Home'
+import { AuthProvider } from '../context/AuthContext';
 
+const
 test('Login page renders with no errors', () => {
     render(<Router>
-            <SnackbarProvider>
-                <Login/>
-            </SnackbarProvider>
-           </Router>)
+            <AuthProvider>
+                <SnackbarProvider>
+                    <Login/>
+                </SnackbarProvider>
+            </AuthProvider>
+           </Router>
+           )
 });
 //test('use jsdom in this test file', () => {
 //  const element = document.createElement('div');
