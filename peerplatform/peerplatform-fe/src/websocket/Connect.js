975c8221c9e0f5d69cf7123eb17376e6bee27a86
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2022-10-09 10:29:02 -0500

    Update Connect.js

--- peerplatform/peerplatform-fe/src/websocket/Connect.js
+++ peerplatform/peerplatform-fe/src/websocket/Connect.js
@@ -16,7 +16,7 @@ class WebSocketService {
         const args = JSON.parse(localStorage.getItem('authTokens'))
         const queryString = args.refresh
         // console.log('what is in querystring', queryString)
-        const path = `ws://codesquad.onrender.com/connect/testing/?${queryString}`;
+        const path = `wss://codesquad.onrender.com/connect/testing/?${queryString}`;
         this.socketRef = new WebSocket(path);
         this.socketRef.onopen = (data) => {
             console.log('WebSocket open');
