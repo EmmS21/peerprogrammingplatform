2048ab03d884f1e243d3f382d981859cc5f029e9
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2023-10-06 05:26:15 -0700

    Update settings.py

--- peerplatform/signup/settings.py
+++ peerplatform/signup/settings.py
@@ -64,6 +64,10 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "https://peerprogrammingplatform.vercel.app",
+]
+
 # CORS_ORIGIN_WHITELIST = (
 #     "localhost",
 #     "https://codesquad.onrender.com",
